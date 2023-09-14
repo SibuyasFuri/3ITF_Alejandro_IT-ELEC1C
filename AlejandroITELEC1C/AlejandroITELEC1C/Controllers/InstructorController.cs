04d908cae5c10a6f992bc7fa81ccd3b8e9a30340
SibuyasFuri <gianmarcus.alejandro.cics@ust.edu.ph>
2023-09-14 21:26:05 +0800

    Added Main Files

---
+++ AlejandroITELEC1C/AlejandroITELEC1C/Controllers/InstructorController.cs
@@ -0,0 +1,46 @@
+﻿using AlejandroITELEC1C.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlejandroITELEC1C.Controllers
+{
+        public class InstructorController : Controller
+        {
+            List<Instructor> InstructorList = new List<Instructor>()
+        {
+        new Instructor()
+        {
+            InstructorId = 1,
+            InstructorFirstName = "Gabriel", InstructorLastName = "Montano", DateHired = DateTime.Now,
+            InstructorEmail = "gdmontano@ust.edu.ph", InstructorRank = InstructorRank.Instructor
+        },
+        new Instructor()
+        {
+            InstructorId = 2,
+            InstructorFirstName = "Leo", InstructorLastName = "Lintag", DateHired = DateTime.Parse("25/2/2002"),
+            InstructorEmail = "Llintag@ust.edu.ph", InstructorRank = InstructorRank.AssistantProfessor
+        },
+        new Instructor()
+        {
+            InstructorId = 3,
+            InstructorFirstName = "Zyx", InstructorLastName = "Lintag", DateHired = DateTime.Parse("25/3/2000"),
+            InstructorEmail = "zmontano@ust.edu.ph", InstructorRank = InstructorRank.Professor
+        },
+        };
+
+        public IActionResult Index()
+        {
+
+            return View(InstructorList);
+        }
+        public IActionResult ShowDetail(int id)
+        {
+            //Search for the instructors whose id matches the given id
+            Instructor? instructor = InstructorList.FirstOrDefault(st => st.InstructorId == id);
+
+            if (instructor != null)//was an instructor found?
+                return View(instructor);
+
+            return NotFound();
+        }
+    }
+    }
---
+++ AlejandroITELEC1C/AlejandroITELEC1C/Models/Instructor.cs
@@ -0,0 +1,21 @@
+﻿using AlejandroITELEC1C.Models;
+
+namespace AlejandroITELEC1C.Models
+{
+    public enum InstructorRank
+    {
+        Instructor, AssistantProfessor, AssociateProfessor, Professor
+    }
+
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string InstructorFirstName { get; set; }
+        public string InstructorLastName { get; set; }
+        public Boolean IsTenured { get; set; }
+        public DateTime DateHired { get; set; }
+        public InstructorRank InstructorRank { get; set; }
+        public string InstructorEmail { get; set; }
+
+    }
+}
